86015c8bf680dd82e48d2002c8a34927144b5689
Tom Machado <tmachad3@gmail.com>
2019-05-28 21:26:15 -0400

    Added routing and set up root for centering content vertically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeFieldComponent } from './welcome-field/welcome-field.component';
+
+const appRoutes: Routes = [
+  { path: '', component: WelcomeFieldComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeFieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
