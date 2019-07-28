c2717638152c75a60bd2ddda0ff85776d9ca630b
Tom Machado <tmachad3@gmail.com>
2019-07-28 12:57:47 -0400

    Improved readability of owned games route

--- backend/api/steam/player.js
+++ backend/api/steam/player.js
@@ -11,7 +11,7 @@ const urlRoot = 'http://api.steampowered.com/IPlayerService';
 router.route('/ownedgames').get(parseUrlEncoded, parseJSON, (req, res) => {
     try {
         url = `${urlRoot}/GetOwnedGames/v1?key=${config.steam.apiKey}&format=json&input_json={"steamid":${req.params.steamid},"include_appinfo":true,"include_played_free_games":true}`;
-        request(url, (err, response, body) => {
+        request.get(url, {json: true}, (err, response, body) => {
             if (err) {
                 throw err;
             } else {
