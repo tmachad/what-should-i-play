07235cc7649e72d48ee356a89e4785b934248f0a
Tom Machado <tmachad3@gmail.com>
2019-08-04 16:04:38 -0400

    Added route to get player info

--- backend/api/steam/player.js
+++ backend/api/steam/player.js
@@ -6,16 +6,16 @@ const parseJSON = bodyParser.json();
 const request = require('request');
 const config = require.main.require('./config.json');
 
-const urlRoot = 'http://api.steampowered.com/IPlayerService';
+const urlRoot = 'http://api.steampowered.com';
 
 router.route('/ownedgames').get(parseUrlEncoded, parseJSON, (req, res) => {
     try {
-        url = `${urlRoot}/GetOwnedGames/v1?key=${config.steam.apiKey}&format=json&input_json={"steamid":${req.params.steamid},"include_appinfo":true,"include_played_free_games":true}`;
+        url = `${urlRoot}/IPlayerService/GetOwnedGames/v1?key=${config.steam.apiKey}&format=json&input_json={"steamid":${req.params.steamid},"include_appinfo":true,"include_played_free_games":true}`;
         request.get(url, {json: true}, (err, response, body) => {
             if (err) {
                 throw err;
             } else if (!body || !body.response || !body.response.games) {
-                console.log('something is undefined, possibly rate limited? sending empty games array');
+                console.log('Something is undefined, possibly rate limited? Sending empty games array.');
                 console.log(body);
                 res.send([]);
             } else {
@@ -28,6 +28,31 @@ router.route('/ownedgames').get(parseUrlEncoded, parseJSON, (req, res) => {
                     });
                 }
                 res.send(games);
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+});
+
+router.get(parseUrlEncoded, parseJSON, (req, res) => {
+    try {
+        url = `${urlRoot}/ISteamUser/GetPlayerSummaries/v0002?key=${config.steam.apiKey}&steamids=${req.params.steamid}`;
+        request.get(url, {json: true}, (err, response, body) => {
+            if (err) {
+                throw err;
+            } else if (!body || !body.response || !body.response.players || !body.response.players[0]) {
+                console.log('Something is undefined, possibly rate limited? Sending null player.');
+                console.log(body);
+                res.send(null);
+            } else {
+                const player = body.response.players[0];
+                res.send({
+                    steamId: player.steamid,
+                    profileName: player.personaname,
+                    public: player.communityvisibilitystate === 3
+                });
             }
         });
     } catch (err) {
